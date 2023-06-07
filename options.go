7e6f656ab44fb1ad2ffd44f7478100c6581488cb
Michael Whatcott <mike@smarty.com>
2023-06-07 10:20:57 -0600

    Non-generic functional options (behind singleton 'namespace' struct).
    Now you have to define a DTO var and pass a pointer in. :(
    
    This is what I was trying to avoid in the initial commit with the
    package-level functions, and as already stated in the previous commit
    message, the 3 options currently provided are all mutually exclusive.

--- options.go
+++ options.go
@@ -9,11 +9,11 @@ import (
 	"os"
 )
 
-func New[T any](options ...func(*config[T])) (dto T, err error) {
+func New(pointer any, options ...func(*config)) error {
 	if len(options) == 0 {
-		options = append(options, OptionJSONFileViaCLI[T]("config", "config.json"))
+		options = append(options, Options.JSONFileViaCLI("config", "config.json"))
 	}
-	var c config[T]
+	var c config
 	for _, option := range options {
 		option(&c)
 	}
@@ -25,32 +25,35 @@ func New[T any](options ...func(*config[T])) (dto T, err error) {
 	if len(c.filename) > 0 {
 		raw, err := os.ReadFile(c.filename)
 		if err != nil {
-			return dto, err
+			return err
 		}
 		c.reader = bytes.NewReader(raw)
 	}
 	if c.reader == nil {
-		return dto, ErrMissingSource
+		return ErrMissingSource
 	}
-	err = json.NewDecoder(c.reader).Decode(&dto)
-	return dto, err
+	return json.NewDecoder(c.reader).Decode(pointer)
 }
 
-type config[T any] struct {
+var Options singleton
+
+type config struct {
 	flag     string
 	flagFile string
 	filename string
 	reader   io.Reader
 }
 
-func OptionJSONReader[T any](r io.Reader) func(*config[T]) {
-	return func(c *config[T]) { c.reader = r }
+type singleton struct{}
+
+func (singleton) JSONReader(r io.Reader) func(*config) {
+	return func(c *config) { c.reader = r }
 }
-func OptionJSONFile[T any](f string) func(*config[T]) {
-	return func(c *config[T]) { c.filename = f }
+func (singleton) JSONFile(f string) func(*config) {
+	return func(c *config) { c.filename = f }
 }
-func OptionJSONFileViaCLI[T any](flag, defaultFilename string) func(*config[T]) {
-	return func(c *config[T]) { c.flag, c.flagFile = flag, defaultFilename }
+func (singleton) JSONFileViaCLI(flag, defaultFilename string) func(*config) {
+	return func(c *config) { c.flag, c.flagFile = flag, defaultFilename }
 }
 
 var ErrMissingSource = errors.New("missing config file source")
--- options_test.go
+++ options_test.go
@@ -11,22 +11,25 @@ import (
 )
 
 func TestOptionJSONReader_NilReader(t *testing.T) {
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONReader[ExampleConfig](nil),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONReader(nil),
 	)
 	should.So(t, err, should.NOT.BeNil)
 	should.So(t, c, should.Equal, ExampleConfig{})
 }
 func TestOptionJSONReader_BadJSON(t *testing.T) {
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONReader[ExampleConfig](bytes.NewBufferString(`BAD JSON`)),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONReader(bytes.NewBufferString(`BAD JSON`)),
 	)
 	should.So(t, c, should.Equal, ExampleConfig{})
 	should.So(t, err, should.NOT.BeNil)
 }
 func TestOptionJSONReader_GoodJSON(t *testing.T) {
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONReader[ExampleConfig](bytes.NewBufferString(`{"message": "Hello, world!"}`)),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONReader(bytes.NewBufferString(`{"message": "Hello, world!"}`)),
 	)
 	should.So(t, err, should.BeNil)
 	should.So(t, c, should.Equal, ExampleConfig{Message: "Hello, world!"})
@@ -34,31 +37,35 @@ func TestOptionJSONReader_GoodJSON(t *testing.T) {
 
 func TestOptionJSONFile_BadJSON(t *testing.T) {
 	_, here, _, _ := runtime.Caller(0)
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONFile[ExampleConfig](filepath.Join(filepath.Dir(here), "config-bad.json")),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONFile(filepath.Join(filepath.Dir(here), "config-bad.json")),
 	)
 	should.So(t, c, should.Equal, ExampleConfig{})
 	should.So(t, err, should.NOT.BeNil)
 }
 func TestOptionJSONFile_GoodJSON(t *testing.T) {
 	_, here, _, _ := runtime.Caller(0)
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONFile[ExampleConfig](filepath.Join(filepath.Dir(here), "testdata", "config-good.json")),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONFile(filepath.Join(filepath.Dir(here), "testdata", "config-good.json")),
 	)
 	should.So(t, err, should.BeNil)
 	should.So(t, c, should.Equal, ExampleConfig{Message: "Hello, world!"})
 }
 
 func TestOptionJSONFileViaCLI_BadJSON(t *testing.T) {
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONFileViaCLI[ExampleConfig]("config", "testdata/config-bad.json"),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONFileViaCLI("config", "testdata/config-bad.json"),
 	)
 	should.So(t, c, should.Equal, ExampleConfig{})
 	should.So(t, err, should.NOT.BeNil)
 }
 func TestOptionJSONFileViaCLI_GoodJSON(t *testing.T) {
-	c, err := configfile.New[ExampleConfig](
-		configfile.OptionJSONFileViaCLI[ExampleConfig]("config", "testdata/config-good.json"),
+	var c ExampleConfig
+	err := configfile.New(&c,
+		configfile.Options.JSONFileViaCLI("config", "testdata/config-good.json"),
 	)
 	should.So(t, err, should.BeNil)
 	should.So(t, c, should.Equal, ExampleConfig{Message: "Hello, world!"})
