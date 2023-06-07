cd2f5fc47c5f577da10450f2a5ef4ce2f60fd561
Michael Whatcott <mike@smarty.com>
2023-06-07 11:36:40 -0600

    Revert "The hard-coded default option prevents this from ever happening."
    This reverts commit da5f62e710aa0e0b957cfca0485e1a8cc5c457ac.
    
    In the case that a nil reader is passed, we do need this.

--- options.go
+++ options.go
@@ -29,6 +29,9 @@ func New(pointer any, options ...func(*config)) error {
 		}
 		c.reader = bytes.NewReader(raw)
 	}
+	if c.reader == nil {
+		return ErrMissingSource
+	}
 	return json.NewDecoder(c.reader).Decode(pointer)
 }
 
