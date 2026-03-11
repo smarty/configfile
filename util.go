d9bbd74e8edc7114e011770acf805776fd262bb6
Michael Whatcott <mike@smarty.com>
2026-03-11 14:34:38 -0600

    Allow 'go fix' to inline this function.
    https://go.dev/blog/inliner

--- util.go
+++ util.go
@@ -2,7 +2,10 @@ package configfile
 
 import "cmp"
 
+// Coalesce returns the first non-zero value, or if none were provided, the zero value.
 // Deprecated
+//
+//go:fix inline
 func Coalesce[T comparable](values ...T) T {
 	return cmp.Or(values...)
 }
